d57a04dd72fb7acd2da9a17a88dd3487476c3687
Donald Merand <dmerand@explo.org>
2020-07-10 15:11:00 -0400

    last select update i promise

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCssHelper.MixProject do
   def project do
     [
       app: :ex_css_helper,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
